bb7e582b50202cdcfae0c98bb28ca2d2c588fd7d
Ruby Reed <rubysreed@gmail.com>
2022-01-17 09:50:50 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Ruby</h1>
+        <h2>Work History</h2>
+          <h3>Babysitter</h3>
+            <p>Take care of kids</p>
     </div>
   );
 }
